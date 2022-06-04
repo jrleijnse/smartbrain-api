ccb01063ba046fd5bd8afa9495ccedb819843065
jrleijnse <jr_leijnse@live.nl>
2022-06-04 15:08:59 +0200

    halp

--- server.js
+++ server.js
@@ -8,13 +8,15 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
-  // connect to your own database here:
   client: 'pg',
   connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl: true,
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+          rejectUnauthorized: false
+        }
   }
 });
 
